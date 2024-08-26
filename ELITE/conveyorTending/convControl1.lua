6a4b01051b5222fc4ea6fc8b91d2d87e1cedfc5b
LuisCMoreira <luiscarlos.f.moreira@gmail.com>
2024-08-26 12:30:42 +0100

    conveyor update

--- ELITE/conveyorTending/convControl1.lua
+++ ELITE/conveyorTending/convControl1.lua
@@ -18,8 +18,8 @@ robotDone="B002" -- internal variable to get if robot finished work
 -- Inetialization ----------------------------------------------------------------------------------
 -- Script Variables
 local stateValue=0 -- value of state
-local startDelay = 3 -- Delay value for the start
-local notWorkDelay = 1 -- Delay value for the start
+local startDelay = 5 -- Delay value for the start
+local lockDelay = 1 -- Delay value for the start
 local stateMachNum = 1 -- number for the conveyor secttion 
 
 
@@ -34,29 +34,38 @@ while(1)do
     set_robot_io_status (bufferStop,0)
 
     while get_robot_io_status("M550")==1 and get_robot_io_status("M417")==1 do
-        elite_print("Running")
-        if stateValue==0 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+        -- elite_print("Running")
+        if stateValue==-1 then
+            -- elite_print("in State ", stateValue," on section: ", stateMachNum)
 
 
 
-            -- wait 3 seconds
+            -- wait x3 seconds
             sleep (startDelay)
 
             if get_robot_io_status (workIn)==0 then 
                 -- set variable for the robot to start
                 set_robot_io_status (bufferStop,1)
+				sleep (lockDelay)
             end
 			
-			sleep (notWorkDelay)
+			
             set_robot_io_status (bufferStop,0)
 			-- set next case
-			stateValue=1
+			stateValue=0
+
+        elseif stateValue==0 then
+            sleep (startDelay)
+            if get_robot_io_status (workIn)==0 then
+                stateValue=-1 
+            else
+                stateValue=1 
+            end
 
 
 
         elseif stateValue==1 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+            -- elite_print("in State ", stateValue," on section: ", stateMachNum)
             -- if work area has part
             if get_robot_io_status (workIn)==1 then 
                 -- set variable for the robot to start
@@ -70,7 +79,7 @@ while(1)do
             end
 
         elseif stateValue==2 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+            -- elite_print("in State ", stateValue," on section: ", stateMachNum)
             
             -- reinitializa robotdone variable
             set_global_variable (robotDone,0)
@@ -83,43 +92,46 @@ while(1)do
                     set_robot_io_status (workStop,1)
                     -- initializa the ready to work variable
                     set_global_variable (readyToWork,0)
+                    stateValue=3
                 end
             end
+        elseif stateValue==3 then
 
             -- if the is no  part in the work position
             if get_robot_io_status (workIn)==0 then
                 -- close the locker
+				sleep (lockDelay)
                 set_robot_io_status (workStop,0)
                 -- set new state
-                stateValue=3
+                stateValue=4
             end
             
-        elseif stateValue==3 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+        elseif stateValue==4 then
+            -- elite_print("in State ", stateValue," on section: ", stateMachNum)
 
             -- if there is a part in the buffer
             if get_robot_io_status (bufferOut)==1 then
                 -- open bufer locker
                 set_robot_io_status (bufferStop,1)
                 --set new state
-                stateValue=4
+                stateValue=5
             end
 
 
-        elseif stateValue==4 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+        elseif stateValue==5 then
+            -- elite_print("in State ", stateValue," on section: ", stateMachNum)
             -- if there is no part in the bufer
             if get_robot_io_status (bufferOut)==0 then
                 -- close the buffer
+				sleep (lockDelay)
                 set_robot_io_status (bufferStop,0)
                 -- go back to initial state
                 stateValue=1
             end
-
         else
             -- unknown state reached
             elite_print("Read error")
-            stateValue=0
+            stateValue=-1
         
         end
 
--- ELITE/conveyorTending/convControl2.lua
+++ ELITE/conveyorTending/convControl2.lua
@@ -14,7 +14,7 @@ local workStop="O1" -- locking actuator of work position output
 -- second robot setup
 -- global variables
 readyToWork="O5" -- inout to set if the work position is ready for the robot to work
-robotDone="I10" -- input to get if robot finished work
+robotDone="I13" -- input to get if robot finished work
 
 hasRobot ="B000" -- if this variable is on, there is a second robot
 
@@ -22,14 +22,14 @@ hasRobot ="B000" -- if this variable is on, there is a second robot
 -- Script Variables
 local stateValue=0 -- value of state
 local startDelay = 5 -- Delay value for the start
-local notWorkDelay = 1 -- Delay value for the start
+local lockDelay = 1 -- Delay value for the start
 local stateMachNum = 2 -- number for the conveyor secttion 
 
 
 
 while(1)do
     
-    elite_print("Ready")
+    -- elite_print("Ready")
     stateValue=0
     -- Initialization of variables
     set_global_variable (readyToWork,0)
@@ -38,24 +38,36 @@ while(1)do
     set_robot_io_status (bufferStop,0)
 
     while get_robot_io_status("M550")==1 and get_robot_io_status("M417")==1 do
-        elite_print("Running")
-        if stateValue==0 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
-            -- wait 5 seconds
+        -- elite_print("Running")
+        if stateValue==-1 then
+            --elite_print("in State ", stateValue," on section: ", stateMachNum)
+
             sleep (startDelay)
 
             if get_robot_io_status (workIn)==0 then 
                 -- set variable for the robot to start
                 set_robot_io_status (bufferStop,1)
+				sleep (lockDelay)
             end
 			
-			sleep (notWorkDelay)
+            -- wait x seconds
+            
             set_robot_io_status (bufferStop,0)
 			-- set next case
-			stateValue=1
+			stateValue=0
+
+        elseif stateValue==0 then
+            sleep (startDelay)
+            if get_robot_io_status (workIn)==0 then
+                stateValue=-1 
+            else
+                stateValue=1 
+            end
+
 
         elseif stateValue==1 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+            --elite_print("in State ", stateValue," on section: ", stateMachNum)
+            
             -- if work area has part
             if get_robot_io_status (workIn)==1 then 
                 -- set variable for the robot to start
@@ -69,7 +81,7 @@ while(1)do
             end
 
         elseif stateValue==2 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+            --elite_print("in State ", stateValue," on section: ", stateMachNum)
             
             -- reinitializa robotdone variable
             set_robot_io_status (robotDone,0)
@@ -82,53 +94,62 @@ while(1)do
                     set_robot_io_status (workStop,1)
                     -- initializa the ready to work variable
                     set_robot_io_status (readyToWork,0)
+                    stateValue=3
                 end
             end
+
+        elseif stateValue==3 then 
+            --elite_print("in State ", stateValue," on section: ", stateMachNum)
+			
 
             -- if the is no  part in the work position
             if get_robot_io_status (workIn)==0 then
                 -- close the locker
+                sleep (lockDelay)
                 set_robot_io_status (workStop,0)
                 -- set new state
-                stateValue=3
+                stateValue=4
             end
             
-        elseif stateValue==3 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
+        elseif stateValue==4 then 
+            --elite_print("in State ", stateValue," on section: ", stateMachNum)
 
             -- if there is a part in the buffer
             if get_robot_io_status (bufferOut)==1 then
                 -- open bufer locker
                 set_robot_io_status (bufferStop,1)
+
                 --set new state
-                stateValue=4
+                stateValue=5
             end
 			
-            if get_robot_io_status (bufferOut)==0 then
-                -- close the buffer
-                set_robot_io_status (bufferStop,0)
-                -- go back to initial state
-                stateValue=1
-            end
-
-
-        elseif stateValue==4 then
-            elite_print("in State ", stateValue," on section: ", stateMachNum)
-            -- if there is no part in the bufer
             --if get_robot_io_status (bufferOut)==0 then
-            --    -- close the buffer
-            --    set_robot_io_status (bufferStop,0)
-            --    -- go back to initial state
-            --    stateValue=1
+                -- close the buffer
+            --    sleep (lockDelay)
+             --   set_robot_io_status (bufferStop,0)
+                -- go back to initial state
+               -- stateValue=1
             --end
+
+
+        elseif stateValue==5 then
+            --elite_print("in State ", stateValue," on section: ", stateMachNum)
+            -- if there is no part in the bufer
+            if get_robot_io_status (bufferOut)==0 then
+            --    -- close the buffer
+				sleep (lockDelay)
+                set_robot_io_status (bufferStop,0)
+            --    -- go back to initial state
+				stateValue=1
+            end
 			
-			stateValue=1
+			--stateValue=1
 
         else
-            -- unknown state reached
+            --- unknown state reached
             elite_print("Read error")
-            stateValue=0
-        
+            stateValue=-1
+       
         end
 
         sleep (0.1) 
